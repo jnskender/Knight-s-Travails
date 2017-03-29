75b0c29590c1f2300b3680703c6e206380732179
John Skender <jnskender@gmail.com>
2017-03-29 18:38:51 -0500

    Add children to Node class

--- Node.rb
+++ Node.rb
@@ -1,9 +1,10 @@
 class Node
 
-  attr_accessor :location,:parent
+  attr_accessor :location,:parent,:children
 
   def initialize(location)
     @parent = nil
     @location = location
+    @children = []
   end
 end
